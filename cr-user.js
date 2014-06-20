61d9459eeaf4a81cc7befee47d94c010ca4f45c7
Gianluca Arbezzano <gianarb92@gmail.com>
2014-06-20 08:40:24 +0000

    Login and logout method and manage AuthSession

--- cr-user.js
+++ cr-user.js
@@ -4,12 +4,22 @@ angular.module('cr.user', [])
     return {
         setKey: function(sign) {
             $crUser.getAuthHandler().setSign(sign);
+        },
+        login: function(o) {
+            $crUser.getAuthSession().cr-user = o;
+        },
+        logout: function(){
+            delete $crUser.getAuthSession.cr-user;
+        },
+        getIdenty: function() {
+            return $crUser.getAuthSession().cr-user;
         }
     };
 }])
 .provider('$crUser', function() {
     var _config = {
-        authHandler: null   
+        authHandler: null,
+        authSession: null
     };
     
     this.setAuthHandler = function (authHandler) {
@@ -18,6 +28,14 @@ angular.module('cr.user', [])
 
     this.getAuthHandler = function() {
         return _config.authHandler;
+    };
+    
+    this.setAuthSession = function (authSession) {
+        _config.authSession = authSession;
+    };
+
+    this.getAuthSession = function() {
+        return _config.authSession;
     };
     
     this.$get = function() {
